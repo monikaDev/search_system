06c4b9786cb97ffe280dff350f77b80a412ff999
monika <monikag@deqode.com>
2021-02-10 17:45:31 +0530

    Create color constants in Constant

---
+++ App/utilities/Constant.js
@@ -0,0 +1,9 @@
+export const UIColors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  grey: '#595959',
+  separatorGray: '#8C8C8C',
+  backgroundGrey: '#BFBFBF',
+  lightCyan: '#F0FFFE',
+  lightGray: '#A09EBB',
+};
