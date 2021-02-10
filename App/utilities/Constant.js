08a082e81deeac09f2e599c232ba93c3da62379a
monika <monikag@deqode.com>
2021-02-10 18:00:11 +0530

    Update color codes

--- App/utilities/Constant.js
+++ App/utilities/Constant.js
@@ -1,9 +1,10 @@
 export const UIColors = {
   white: '#FFFFFF',
   black: '#000000',
-  grey: '#595959',
+  transparent: 'transparent',
+  gray: '#595959',
   separatorGray: '#8C8C8C',
-  backgroundGrey: '#BFBFBF',
+  backgroundGray: '#322F2F',
   lightCyan: '#F0FFFE',
   lightGray: '#A09EBB',
 };
