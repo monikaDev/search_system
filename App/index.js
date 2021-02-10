f65e64a086d15ea613c91f45aa2390414a79de76
monika <monikag@deqode.com>
2021-02-10 14:32:26 +0530

    Create App folder and add index file

---
+++ App/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}></ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+});
+
+export default App;
