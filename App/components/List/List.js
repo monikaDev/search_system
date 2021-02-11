bdf28203260ebbd18d0a275ff680b56350206e61
monika <monikag@deqode.com>
2021-02-11 13:16:59 +0530

    Catch the tabs of list items

--- App/components/List/List.js
+++ App/components/List/List.js
@@ -11,6 +11,7 @@ const List = (props) => {
       data={data}
       showsVerticalScrollIndicator={false}
       ItemSeparatorComponent={() => <ListSeparator />}
+      keyboardShouldPersistTaps="always"
     />
   );
 };
