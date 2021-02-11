13f45c9241c480809282b256bfe2eede0d56bc2b
monika <monikag@deqode.com>
2021-02-11 13:14:04 +0530

    Create validation utils

--- App/components/List/ListItem.js
+++ App/components/List/ListItem.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {UIColors} from '../../utilities/Constant';
+import {isEmptyString} from '../../utilities/validation';
 
 const ListItem = (props) => {
   const {title, updateSearchTerm, ...rest} = props;
 
-  return (
+  return !isEmptyString(title) ? (
     <TouchableOpacity
       {...rest}
       style={styles.container}
       onPress={() => updateSearchTerm(`${title} `)}>
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
-  );
+  ) : null;
 };
 
 const styles = StyleSheet.create({
